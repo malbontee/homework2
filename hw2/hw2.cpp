e8312b96ef0337d26162a1caee11462f37bec00d
malbontee <144820547+malbontee@users.noreply.github.com>
2024-09-11 16:35:55 +0300

    Добавьте файлы проекта.

---
+++ hw2/hw2.cpp
@@ -0,0 +1,114 @@
+﻿
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    /*Написать программу, которая находит среднее арифметическое значение трёх вещественных чисел.*/
+    
+    //double num1, num2, num3;
+    //cout << "Please enter the first number:\n";
+    //cin >> num1;
+    //cout << "Please enter the second number:\n";
+    //cin >> num2;
+    //cout << "Please enter the third number:\n";
+    //cin >> num3;
+    //double result = (num1 + num2 + num3) / 3;
+    //cout << "Your result: " << result << "\n";
+
+    ///*Пользователь вводит число и степень. Программа вычисляет указанную степень этого числа (пригодится функция pow).*/
+    //int number;
+    //cout << "Please enter your number:\n";
+    //cin >> number;
+    //int pow_of_num = 0;
+    //cout << "Please enter your number:\n";
+    //cin >> pow_of_num;
+    //cout << "Your result: " << pow(number, pow_of_num) << "\n";
+    
+    ///*Написать программу, которая  предлагает пользователю ввести радиус окружности, а затем считает площадь и длину этой окружности. Число Pi задать в программе как вещественную константу.*/
+    //const double pi_number = 3.141592653589793;
+    //double radius;
+    //cout << "Please enter radius:\n";
+    //cin >> radius;
+    //double area_circle = pi_number * pow(radius, 2);
+    //double length_circle = 2 * pi_number * radius;
+    //cout << "Area of circle is: " << area_circle << "\n";
+    //cout << "Length of circle is: " << length_circle << "\n";
+
+    ///*Написать программу, которая  предоставляет пользователю возможность ввести с клавиатуры количество гривен, и переводит это количество в доллары, евро и биткоины.*/
+    //double hryvnias;
+    //cout << "Enter your sum of money in HRYVNIAS: ";
+    //cin >> hryvnias;
+    //double res_dollars = hryvnias / 41.4;
+    //double res_euro = hryvnias / 45.9;
+    //double res_crypto = hryvnias / 2403620;
+    //cout << "Your sum in dollars: " << res_dollars << "\n";
+    //cout << "Your sum in euro: " << res_euro << "\n";
+    //cout << "Your sum in cryptocurrency: " << res_crypto << "\n";
+    
+    ///*Преподаватель вводит с клавиатуры количество студентов, сдавших домашние задание, и количество «должников». Посчитать, сколько процентов составляют «должники» от общего числа студентов. */
+    //int good_students;
+    //cout << "Please enter the number of students who have sent all the homework assignments:\n";
+    //cin >> good_students;
+    //int bad_students;
+    //cout << "Please enter the number of students who have debts:\n";
+    //cin >> bad_students;
+    //int all_students = good_students + bad_students;
+    //double res_percent = (static_cast<double>(bad_students) / all_students) * 100;
+    //cout << "Result in percent is: " << res_percent << "%\n";
+
+    ///* Пользователь вводит с клавиатуры сумму в гривнах, срок вклада в месяцах и процентную ставку за год. Посчитать, сколько денег будет у пользователя в конце срока. */
+    //int sum, term;
+    //cout << "Enter your sum of money in HRYVNIAS:\n";
+    //cin >> sum;
+    //cout << "Enter the term of the deposit in months:\n";
+    //cin >> term;
+    //double rate;
+    //cout << "Enter the annual percentage rate:\n";
+    //cin >> rate;
+    //double res = sum + sum * (rate / 100 * term / 12);
+    //cout << "Your result:" << res << "\n";
+
+    ///*Ввести с клавиатуры 3-значное число и «перевернуть» его (например, ввести 357 – получить и вывести 753).*/
+    //int number;
+    //cout << "Please enter your THREE-digit number:\n";
+    //cin >> number;
+    //int last_digit = number % 10;
+    //int second_digit = (number / 10) % 10;
+    //int first_digit = number / 100;
+    //cout << last_digit << second_digit << first_digit << " - your reversed number! :)\n";
+
+    ///*Ввести с клавиатуры 3-значное число и удалить из него среднюю цифру.*/
+    //int number;
+    //cout << "Please enter your THREE-digit number:\n";
+    //cin >> number;
+    //int digit_last = number % 10;
+    //int digit_first = number / 100;
+    //cout << digit_first << digit_last << " - your result!\n";
+
+    ///*Пользователь вводит с клавиатуры сумму в гривнах (дробное число). Вывести по отдельности количество гривен и количество копеек (например, ввести 45.67 гривен - получить и вывести 45 гривен 67 копеек)*/
+    //double sum_money;
+    //cout << "Please enter your sum of money in HRYVNIAS and with KOPECKS:\n";
+    //cin >> sum_money;
+    //int hryvnias = static_cast<int>(sum_money);
+    //int kopecks = static_cast<int>(round((sum_money - hryvnias) * 100));
+    //cout << hryvnias << " hryvnias and " << kopecks << " kopecks\n";
+    //
+    ///*Ввести с клавиатуры два 3-значных числа и поменять у них средние цифры (например, ввести 357  и 702 – получить и вывести числа  307 и 752).*/
+    //int num1, num2;
+    //cout << "Please enter your THREE-digit number:\n";
+    //cin >> num1;
+    //cout << "Please enter your THREE-digit number:\n";
+    //cin >> num2;
+    //int num1_last = num1 % 10;
+    //int num1_second = (num1 / 10) % 10;
+    //int num1_first = num1 / 100;
+    //int num2_last = num2 % 10;
+    //int num2_second = (num2 / 10) % 10;
+    //int num2_first = num2 / 100;
+    //cout << num1_first << num2_second << num1_last << " - the first number with the second digit replaced!\n";
+    //cout << num2_first << num1_second << num2_last << " - the second number with the second digit replaced!\n";
+}
+
